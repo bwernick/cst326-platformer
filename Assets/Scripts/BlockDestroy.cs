81e11f9ac370c7ce13dcbb3aa954d85f62c1213a
bwernick <bwernick@csumb.edu>
2020-02-16 15:42:49 -0800

    UI & Raycast
    UI in global space.
    Physics.Raycast implemented to destroy blocks on click.
    Coins block destruction increaments coin counter on UI.
    Timer in UI counts down from 300.
    Camera can move with WASD for debug purposes.

---
+++ Assets/Scripts/BlockDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockDestroy : MonoBehaviour
+{
+    private float distance = 10;
+    private float offset = -1;
+    private int coins = 0;
+
+    public Text coinText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if(hit.transform.gameObject.name == "question(Clone)")
+                {
+                    coins += 1;
+                    coinText.text = "" + coins;
+                }
+
+                Debug.Log("Destroyed" + hit.transform.gameObject.name);
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour
+{
+    float timeLeft = 300.0f;
+
+    public Text text;
+
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        text.text = "" + Mathf.Round(timeLeft);
+        if (timeLeft < 0)
+        {
+            Debug.Log("Game Over");
+        }
+    }
+}
